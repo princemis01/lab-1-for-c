099cfc76f3f2e28a1824e2fcabda9a24028b6be5
Prince Mishra <157045372+princemis01@users.noreply.github.com>
2024-09-19 16:02:14 -0400

    lab 1

---
+++ numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Integer
+    int myNum = 1000;
+    printf("%d\n", myNum);
+
+    // Float
+    float floatMyNum = 5.75;
+    printf("%f\n", floatMyNum);
+
+    // Double
+    double doubleMyNum = 19.99;
+    printf("%lf\n", doubleMyNum);
+
+    return 0;
+}
---
+++ reallife.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int studentID = 15;
+    int studentAge = 23;
+    float studentFee = 75.25;
+    char studentGrade = 'B';
+
+    printf("Student ID: %d\n", studentID);
+    printf("Student Age: %d\n", studentAge);
+    printf("Student Fee: %.2f\n", studentFee);
+    printf("Student Grade: %c\n", studentGrade);
+
+    return 0;
+}
+
+
+
